ac0a3d0db7bbaef9468ed8f61fa793e63d6fe071
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-05-21 10:08:05 +0100

    Update app.js
    Changed app.js so it contains only functionality used in the all sections of the website

--- app.js
+++ app.js
@@ -1,28 +1,75 @@
-let text1 = document.getElementById('text1anim');
-let text2 = document.getElementById('text2anim');
-let text3 = document.getElementById('text3anim')
-let text4 = document.getElementById('text4anim')
 
 let	header = document.getElementById('header');
-let backgroundBlack = document.getElementById('backgroundBlack');
+let backgroundImage = document.getElementById('content');
+let name = document.getElementById('headerLeft')
+let copyrights = document.getElementById('copyrights')
+let spanMonth = document.getElementById('month')
+let spanDay = document.getElementById('day')
+let spanYear = document.getElementById('year')
+let photoDiv = document.getElementById('photoDiv')
+let barsBtn = document.getElementById('bars')
+let dropMenu = document.getElementById('dropDownDiv')
+let dropMenuLeft = document.getElementById('dropDownLeft')
+let langIcons = document.getElementById('langIcons')
 
-//set animation effect for the header and set text2 class name
+
+let date = new Date;
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = `${date.getDate()}`.padStart(2, "0");
+
+let monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+month = monthName[month];
+spanMonth.innerHTML = month;
+spanDay.innerHTML = day;
+spanYear.innerHTML = year;
+
+copyrights.innerHTML = `&copy; Copyright ${year}, Pawel Krzesinski`
+
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
-	setTimeout(() => header.className = 'headerSlide', 0)
-	text2.className = 'text2';
-})
+	//set animation effects
+	setTimeout(() => name.className = 'headerNameAppear', 500)
+	setTimeout(() => header.style.animation = "animation: slideRight 0.2s ease", 0)
+	//takes the dark filter on the left of drop menu out of the screen
+	dropMenuLeft.style.transform = "translate3d(-100%, 0, 0)"
 
-// change text2 div class so it slides from it's initial position to the specified position
-window.addEventListener('scroll', () => {
-		if(text1.getBoundingClientRect().bottom <= 0) {
-			text2.className = "text2show";
-		}
 })
-// hides the header when scrolling through black background
+//gets all the icons in the langIcons div
+let icons = langIcons.querySelectorAll('img');
+function iconsAppear() {
+	//makes icons variable content in to a proper array and gives it an animation effect
+	Array.from (icons)
+		.forEach(icon => icon.style.animation = 'appear 1.5s linear')
+}
+
 window.addEventListener('scroll', () => {
-		if(backgroundBlack.getBoundingClientRect().top <= 0 && backgroundBlack.getBoundingClientRect().bottom >= 0 ) {
+	// hides the header when scrolling through background
+		if(backgroundImage.getBoundingClientRect().top <= 0 && backgroundImage.getBoundingClientRect().bottom >= 0 ) {
 			header.style.visibility = "hidden";
 		} else {
 			header.style.visibility = "visible";
 		}
+		//makes the icons on the bottom appear when scrolling 
+		if(langIcons.getBoundingClientRect().top <= 1000) {
+			iconsAppear();
+		}
+	
+})
+
+//adds functionality to bars icon when in mobile/zoomed in mode
+barsBtn.addEventListener('click', () => {
+	if(dropMenu.className === 'dropDownDiv'){
+		dropMenu.className = 'dropDownDivOn';
+		dropMenuLeft.className = "dropDownLeftA"
+		dropMenuLeft.style.transform = "none";
+	} else {
+		dropMenu.className = 'dropDownDiv';
+		dropMenuLeft.className = "dropDownLeftB"
+		setTimeout(() => {
+			dropMenuLeft.style.transform = "translate3d(-100%, 0, 0)";
+		}, 500)
+	}
 })
