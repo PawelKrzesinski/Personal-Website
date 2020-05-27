2ff4ee11dbf14f1951023c3854c0ced2139e13c4
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-05-27 23:37:51 +0100

    Update app.js
    -removed not used constants
    -changed class and id naming
    -removed "header hidden when scrolling through background"
    -added animation to the drop down menu when closing.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
-let	header = document.getElementById('header');
-let backgroundImage = document.getElementById('content');
-let name = document.getElementById('headerLeft')
-let copyrights = document.getElementById('copyrights')
-let spanMonth = document.getElementById('month')
-let spanDay = document.getElementById('day')
-let spanYear = document.getElementById('year')
-let photoDiv = document.getElementById('photoDiv')
-let barsBtn = document.getElementById('bars')
-let dropMenu = document.getElementById('dropDownDiv')
-let dropMenuLeft = document.getElementById('dropDownLeft')
-let langIcons = document.getElementById('langIcons')
+const	header = document.getElementById('header');
+const background_image = document.getElementById('background');
+const name = document.getElementById('header_left')
+const copyrights = document.getElementById('copyrights')
+const span_month = document.getElementById('month')
+const span_day = document.getElementById('day')
+const span_year = document.getElementById('year')
+const bars_btn = document.getElementById('bars')
+const drop_menu = document.getElementById('drop_down_div')
+const drop_menu_left = document.getElementById('drop_down_left')
+const lang_icons = document.getElementById('lang_icons')
+
 
 
 let date = new Date;
@@ -18,58 +18,58 @@ let year = date.getFullYear();
 let month = date.getMonth();
 let day = `${date.getDate()}`.padStart(2, "0");
 
-let monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-month = monthName[month];
-spanMonth.innerHTML = month;
-spanDay.innerHTML = day;
-spanYear.innerHTML = year;
+let month_name = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+month = month_name[month];
+span_month.innerHTML = month;
+span_day.innerHTML = day;
+span_year.innerHTML = year;
 
 copyrights.innerHTML = `&copy; Copyright ${year}, Pawel Krzesinski`
 
 
 
 
+
+
 window.addEventListener('DOMContentLoaded', () => {
 	//set animation effects
-	setTimeout(() => name.className = 'headerNameAppear', 500)
-	setTimeout(() => header.style.animation = "animation: slideRight 0.2s ease", 0)
+	setTimeout(() => name.className = 'header_name_appear', 500)
+	setTimeout(() => header.style.animation = "animation: slide_right 0.2s ease", 0)
 	//takes the dark filter on the left of drop menu out of the screen
-	dropMenuLeft.style.transform = "translate3d(-100%, 0, 0)"
+	drop_menu_left.style.transform = "translate3d(-100%, 0, 0)"
 
 })
 //gets all the icons in the langIcons div
-let icons = langIcons.querySelectorAll('img');
-function iconsAppear() {
+let icons = lang_icons.querySelectorAll('img');
+function icons_appear() {
 	//makes icons variable content in to a proper array and gives it an animation effect
 	Array.from (icons)
 		.forEach(icon => icon.style.animation = 'appear 1.5s linear')
 }
 
 window.addEventListener('scroll', () => {
-	// hides the header when scrolling through background
-		if(backgroundImage.getBoundingClientRect().top <= 0 && backgroundImage.getBoundingClientRect().bottom >= 0 ) {
-			header.style.visibility = "hidden";
-		} else {
-			header.style.visibility = "visible";
-		}
 		//makes the icons on the bottom appear when scrolling 
-		if(langIcons.getBoundingClientRect().top <= 1000) {
-			iconsAppear();
+		if(lang_icons.getBoundingClientRect().top <= 1000) {
+			icons_appear();
 		}
-	
+
 })
 
 //adds functionality to bars icon when in mobile/zoomed in mode
-barsBtn.addEventListener('click', () => {
-	if(dropMenu.className === 'dropDownDiv'){
-		dropMenu.className = 'dropDownDivOn';
-		dropMenuLeft.className = "dropDownLeftA"
-		dropMenuLeft.style.transform = "none";
+bars_btn.addEventListener('click', () => {
+	if(drop_menu.className === 'drop_down_div_off' || drop_menu.className === 'drop_down_div'){
+		drop_menu.className = 'drop_down_div_on';
+		drop_menu_left.className = "drop_down_left_on";
+		drop_menu_left.style.transform = "none";
+		drop_menu.style.transform = "none";
 	} else {
-		dropMenu.className = 'dropDownDiv';
-		dropMenuLeft.className = "dropDownLeftB"
+		drop_menu.className = 'drop_down_div_off';
+		drop_menu_left.className = "drop_down_left_off";
 		setTimeout(() => {
-			dropMenuLeft.style.transform = "translate3d(-100%, 0, 0)";
-		}, 500)
+			drop_menu.style.transform = "translateX(100%)";
+		}, 0),		
+		setTimeout(() => {
+			drop_menu_left.style.transform = "translateX(-100%)";
+		}, 400)
 	}
 })
