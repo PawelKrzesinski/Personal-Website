7e0867e5a66451c968dcce5b7bab672c8e0495d8
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-06-25 12:52:32 +0100

    Add files via upload
    Contact form animations

---
+++ js/contact.js
@@ -0,0 +1,52 @@
+const label__name = document.getElementById("label__name");
+const label__email = document.getElementById("label__email");
+const label__company = document.getElementById("label__company");
+const label__comment = document.getElementById("label__comment");
+const input__name = document.getElementById("input__name");
+const input__email = document.getElementById("input__email");
+const input__company = document.getElementById("input__company");
+const input__comment = document.getElementById("input__comment");
+const submit = document.getElementById("submit");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	setTimeout(() => {
+		label__name.style.animation = "appear 1s ease-out";
+		label__name.style.opacity = "1";
+	}, 100)
+	setTimeout(() => {
+		input__name.style.animation = "appear 1s ease-out";
+		input__name.style.opacity = "1";
+	}, 200)
+	setTimeout(() => {
+		label__email.style.animation = "appear 1s ease-out";
+		label__email.style.opacity = "1";
+	}, 300)
+	setTimeout(() => {
+		input__email.style.animation = "appear 1s ease-out";
+		input__email.style.opacity = "1";
+	}, 400)
+	setTimeout(() => {
+		label__company.style.animation = "appear 1s ease-out";
+		label__company.style.opacity = "1";
+	}, 500)
+	setTimeout(() => {
+		input__company.style.animation = "appear 1s ease-out";
+		input__company.style.opacity = "1";
+	}, 600)
+	setTimeout(() => {
+		label__comment.style.animation = "appear 1s ease-out";
+		label__comment.style.opacity = "1";
+	}, 700)
+	setTimeout(() => {
+		input__comment.style.animation = "appear 1s ease-out";
+		input__comment.style.opacity = "1";
+	}, 800)
+	setTimeout(() => {
+		submit.style.animation = "appear 1s ease-out";
+		submit.style.opacity = "1";
+	}, 900)
+})
+
+
+
